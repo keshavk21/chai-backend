c485b6b258a0f8e307ded032e7562eebed2db957
Keshav Khandewal <khandelwalkeshav2004@gmail.com>
2024-10-28 15:59:50 +0530

    all setup done

--- src/app.js
+++ src/app.js
@@ -14,4 +14,14 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// routes
+
+import userRouter from './routes/user.routes.js'
+
+// routes declaration
+
+app.use("/api/v1/users", userRouter)
+// htpps://localhost:5000/api/v1/users/register
+
+
 export{app}
